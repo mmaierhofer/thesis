c04656515ebb238bca513096c648d077eb74371e
maierhoferma81177@th-nuernberg.de <mmaierhofer@outlook.de>
2022-07-24 21:01:52 +0200

    update leet

--- leet.py
+++ leet.py
@@ -1,27 +1,34 @@
 
+from typing import List
 from numpy import sort
 
 
-def twoSum(nums):
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def twoSum(list1, list2):
     """
        :type x: int
        :rtype: bool
        """
 
-    rightSum = sum(nums)
-
-    leftSum = 0
-
-    for idx, val in enumerate(nums):
-        if leftSum == rightSum:
-            return idx
-        elif idx == len(nums)-1:
-            return -1
-
-        rightSum -= val
-        leftSum += val
-
-    return rightSum
+    if not list1 and not list2:
+        return list1
+    if not list1 or not list2:
+        return list1 if not list2 else list2
+    seek, target = (list1, list2) if list1.val < list2.val else (list2, list1)
+    head = seek
+    while seek and target:
+        while seek.next and seek.next.val < target.val:
+            seek = seek.next
+        seek.next, target = target, seek.next
+        seek = seek.next
+    return head
 
 
-print(twoSum([2, 1, -1]))
+list1 = list({1, 2, 3})
+
+print(twoSum({1, 2, 3}, {1, 2, 3}))
